adc873c36d4d5e04c9eeed37cbb118419adafcb7
Melly5 <92356658+Melly5@users.noreply.github.com>
2022-11-20 21:44:14 +0700

    7prac

---
+++ 7pracOpenGLLight/7prac/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace _7prac
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+			this.ambientToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.diffuseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.specularToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.emissionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripDropDownButton2 = new System.Windows.Forms.ToolStripDropDownButton();
+			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+			this.simp = new Tao.Platform.Windows.SimpleOpenGlControl();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.toolStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1,
+            this.toolStripDropDownButton2});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(800, 27);
+			this.toolStrip1.TabIndex = 0;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// toolStripDropDownButton1
+			// 
+			this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ambientToolStripMenuItem,
+            this.diffuseToolStripMenuItem,
+            this.specularToolStripMenuItem,
+            this.emissionToolStripMenuItem});
+			this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+			this.toolStripDropDownButton1.Size = new System.Drawing.Size(78, 24);
+			this.toolStripDropDownButton1.Text = "Material";
+			// 
+			// ambientToolStripMenuItem
+			// 
+			this.ambientToolStripMenuItem.Name = "ambientToolStripMenuItem";
+			this.ambientToolStripMenuItem.Size = new System.Drawing.Size(150, 26);
+			this.ambientToolStripMenuItem.Text = "Ambient";
+			this.ambientToolStripMenuItem.Click += new System.EventHandler(this.ambientToolStripMenuItem_Click);
+			// 
+			// diffuseToolStripMenuItem
+			// 
+			this.diffuseToolStripMenuItem.Name = "diffuseToolStripMenuItem";
+			this.diffuseToolStripMenuItem.Size = new System.Drawing.Size(150, 26);
+			this.diffuseToolStripMenuItem.Text = "Diffuse";
+			this.diffuseToolStripMenuItem.Click += new System.EventHandler(this.diffuseToolStripMenuItem_Click);
+			// 
+			// specularToolStripMenuItem
+			// 
+			this.specularToolStripMenuItem.Name = "specularToolStripMenuItem";
+			this.specularToolStripMenuItem.Size = new System.Drawing.Size(150, 26);
+			this.specularToolStripMenuItem.Text = "Specular";
+			this.specularToolStripMenuItem.Click += new System.EventHandler(this.specularToolStripMenuItem_Click);
+			// 
+			// emissionToolStripMenuItem
+			// 
+			this.emissionToolStripMenuItem.Name = "emissionToolStripMenuItem";
+			this.emissionToolStripMenuItem.Size = new System.Drawing.Size(150, 26);
+			this.emissionToolStripMenuItem.Text = "Emission";
+			this.emissionToolStripMenuItem.Click += new System.EventHandler(this.emissionToolStripMenuItem_Click);
+			// 
+			// toolStripDropDownButton2
+			// 
+			this.toolStripDropDownButton2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3});
+			this.toolStripDropDownButton2.Name = "toolStripDropDownButton2";
+			this.toolStripDropDownButton2.Size = new System.Drawing.Size(56, 24);
+			this.toolStripDropDownButton2.Text = "Light";
+			this.toolStripDropDownButton2.Click += new System.EventHandler(this.toolStripDropDownButton2_Click_1);
+			// 
+			// toolStripMenuItem1
+			// 
+			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+			this.toolStripMenuItem1.Text = "X";
+			this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+			// 
+			// toolStripMenuItem2
+			// 
+			this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+			this.toolStripMenuItem2.Size = new System.Drawing.Size(224, 26);
+			this.toolStripMenuItem2.Text = "Y";
+			this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+			// 
+			// toolStripMenuItem3
+			// 
+			this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+			this.toolStripMenuItem3.Size = new System.Drawing.Size(224, 26);
+			this.toolStripMenuItem3.Text = "Z";
+			this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+			// 
+			// simp
+			// 
+			this.simp.AccumBits = ((byte)(0));
+			this.simp.AutoCheckErrors = false;
+			this.simp.AutoFinish = false;
+			this.simp.AutoMakeCurrent = true;
+			this.simp.AutoSwapBuffers = true;
+			this.simp.BackColor = System.Drawing.Color.Black;
+			this.simp.ColorBits = ((byte)(32));
+			this.simp.DepthBits = ((byte)(16));
+			this.simp.Location = new System.Drawing.Point(12, 30);
+			this.simp.Name = "simp";
+			this.simp.Size = new System.Drawing.Size(776, 482);
+			this.simp.StencilBits = ((byte)(0));
+			this.simp.TabIndex = 1;
+			this.simp.KeyDown += new System.Windows.Forms.KeyEventHandler(this.simp_KeyDown);
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 524);
+			this.Controls.Add(this.simp);
+			this.Controls.Add(this.toolStrip1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+		private System.Windows.Forms.ToolStripMenuItem ambientToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem diffuseToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem specularToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem emissionToolStripMenuItem;
+		private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton2;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+		private Tao.Platform.Windows.SimpleOpenGlControl simp;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ 7pracOpenGLLight/7prac/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Tao.OpenGl;
+using Tao.FreeGlut;
+using Tao.Platform.Windows;
+
+namespace _7prac
+{
+	public partial class Form1 : Form
+	{
+		float[,] figure = { { -0.3f, -0.2f, 0.8f }, { 0.3f, -0.2f, 0.8f }, { 0.3f, -0.2f, -0.4f }, { -0.3f, -0.2f, -0.4f }, { -0.3f, 0, 0.8f }, { 0.3f, 0, 0.8f }, { 0.3f, 0, 0 }, { -0.3f, 0, 0 }, { -0.3f, 0.2f, 0 }, { 0.3f, 0.2f, 0 }, { 0.3f, 0.2f, -0.4f }, { -0.3f, 0.2f, -0.4f } };
+		float[] lightX = { 1, 0, 0, 0 };
+		float[] lightY = { 0, 1, 0, 0 };
+		float[] lightZ = { 0, 0, 1, 0 };
+		float[] light_dif = { 0.7f, 0.2f, 0.2f };
+		float[] color = { 0.5f, 0.2f, 1f };
+
+		public Form1()
+		{
+			InitializeComponent();
+			simp.InitializeContexts();
+
+			Gl.glViewport(0, 0, simp.Width, simp.Height);
+			Gl.glClearColor(1, 1, 1, 1);
+			Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+			Gl.glClear(Gl.GL_DEPTH_BUFFER_BIT);
+			Gl.glEnable(Gl.GL_DEPTH_TEST);
+			Gl.glMatrixMode(Gl.GL_PROJECTION);
+			Gl.glLoadIdentity();
+			Gl.glFrustum(-1, 1, -1, 1, 3, 10);
+			Gl.glMatrixMode(Gl.GL_MODELVIEW);
+			Gl.glTranslatef(0, 0, -7);
+			DrawFigure(figure);
+
+			simp.Invalidate();
+
+		}
+
+		void DrawFigure(float[,] a)
+		{
+			Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+			Gl.glClear(Gl.GL_DEPTH_BUFFER_BIT);
+
+
+			//нижняя
+			Gl.glBegin(Gl.GL_POLYGON);
+			Gl.glNormal3f(0.0f, -1.0f, 0.0f);
+			for (int i = 0; i < 4; i++)
+				Gl.glVertex3f(a[i, 0], a[i, 1], a[i, 2]);
+			Gl.glEnd();
+			//над нижней
+			Gl.glBegin(Gl.GL_QUADS);
+			Gl.glNormal3f(0.0f, 1.0f, 0.0f);
+			for (int i = 4; i < 8; i++)
+				Gl.glVertex3f(a[i, 0], a[i, 1], a[i, 2]);
+			Gl.glEnd();
+			//над над нижней
+			Gl.glBegin(Gl.GL_QUADS);
+			Gl.glNormal3f(0.0f, 1.0f, 0.0f);
+			for (int i = 11; i > 7; i--)
+				Gl.glVertex3f(a[i, 0], a[i, 1], a[i, 2]);
+			Gl.glEnd();
+
+			//перед низ
+			Gl.glBegin(Gl.GL_QUADS);
+			Gl.glNormal3f(0.0f, 0.0f, 1.0f);
+			Gl.glVertex3f(a[0, 0], a[0, 1], a[0, 2]);
+			Gl.glVertex3f(a[1, 0], a[1, 1], a[1, 2]);
+			Gl.glVertex3f(a[5, 0], a[5, 1], a[5, 2]);
+			Gl.glVertex3f(a[4, 0], a[4, 1], a[4, 2]);
+			Gl.glEnd();
+			//перед верх
+			Gl.glBegin(Gl.GL_QUADS);
+			Gl.glNormal3f(0.0f, 0.0f, 1.0f);
+			Gl.glVertex3f(a[7, 0], a[7, 1], a[7, 2]);
+			Gl.glVertex3f(a[6, 0], a[6, 1], a[6, 2]);
+			Gl.glVertex3f(a[9, 0], a[9, 1], a[9, 2]);
+			Gl.glVertex3f(a[8, 0], a[8, 1], a[8, 2]);
+			Gl.glEnd();
+			//зад
+			Gl.glBegin(Gl.GL_QUADS);
+			Gl.glNormal3f(0.0f, 0.0f, -1.0f);
+			Gl.glVertex3f(a[3, 0], a[3, 1], a[3, 2]);
+			Gl.glVertex3f(a[2, 0], a[2, 1], a[2, 2]);
+			Gl.glVertex3f(a[10, 0], a[10, 1], a[10, 2]);
+			Gl.glVertex3f(a[11, 0], a[11, 1], a[11, 2]);
+			Gl.glEnd();
+
+			//бок -х
+			Gl.glBegin(Gl.GL_POLYGON);
+			Gl.glNormal3f( -1.0f, 0.0f, 0.0f);
+			Gl.glVertex3f(a[7, 0], a[7, 1], a[7, 2]);
+			Gl.glVertex3f(a[4, 0], a[4, 1], a[4, 2]);
+			Gl.glVertex3f(a[0, 0], a[0, 1], a[0, 2]);
+			Gl.glVertex3f(a[3, 0], a[3, 1], a[3, 2]);
+			Gl.glVertex3f(a[11, 0], a[11, 1], a[11, 2]);
+			Gl.glVertex3f(a[8, 0], a[8, 1], a[8, 2]);
+			Gl.glEnd();
+			//бок х
+			Gl.glBegin(Gl.GL_POLYGON);
+			Gl.glNormal3f(1.0f, 0.0f, 0.0f);
+			Gl.glVertex3f(a[6, 0], a[6, 1], a[6, 2]);
+			Gl.glVertex3f(a[5, 0], a[5, 1], a[5, 2]);
+			Gl.glVertex3f(a[1, 0], a[1, 1], a[1, 2]);
+			Gl.glVertex3f(a[2, 0], a[2, 1], a[2, 2]);
+			Gl.glVertex3f(a[10, 0], a[10, 1], a[10, 2]);
+			Gl.glVertex3f(a[9, 0], a[9, 1], a[9, 2]);
+			Gl.glEnd();
+			simp.Invalidate();
+		}
+		private void Rotation(float x, float y, float z)
+		{
+			Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+			Gl.glClear(Gl.GL_DEPTH_BUFFER_BIT);
+			Gl.glMatrixMode(Gl.GL_MODELVIEW);
+			Gl.glRotatef(10, x, y, z);
+			DrawFigure(figure);
+			simp.Invalidate();
+		}
+
+		private void toolStripDropDownButton2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void ambientToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_AMBIENT, color);
+			simp.Invalidate();
+		}
+
+		private void diffuseToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_DIFFUSE, color);
+			simp.Invalidate();
+		}
+
+		private void specularToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_SPECULAR, color);
+			simp.Invalidate();
+		}
+
+		private void emissionToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_EMISSION, color);
+			simp.Invalidate();
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			Rotation(1, 1, 1);
+		}
+
+		private void simp_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Space)
+				timer1.Start();
+			if (e.KeyCode == Keys.X)
+				timer1.Stop();
+			if (e.KeyCode == Keys.Q)
+				Rotation(1f, 0, 0);
+			if (e.KeyCode == Keys.W)
+				Rotation(-1f, 0, 0);
+			if (e.KeyCode == Keys.E)
+				Rotation(0, 1f, 0);
+			if (e.KeyCode == Keys.R)
+				Rotation(0, -1f, 0);
+			if (e.KeyCode == Keys.T)
+				Rotation(0, 0, 1f);
+			if (e.KeyCode == Keys.Y)
+				Rotation(0, 0, -1f);
+		}
+
+		private void toolStripMenuItem1_Click(object sender, EventArgs e)
+		{
+			Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_DIFFUSE, light_dif);
+			Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, lightX);
+
+		}
+
+		private void toolStripMenuItem2_Click(object sender, EventArgs e)
+		{
+			Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_DIFFUSE, light_dif);
+			Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, lightY);
+
+		}
+
+		private void toolStripMenuItem3_Click(object sender, EventArgs e)
+		{
+			Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_DIFFUSE, light_dif);
+			Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, lightZ);
+
+		}
+
+		private void toolStripDropDownButton2_Click_1(object sender, EventArgs e)
+		{
+			Gl.glEnable(Gl.GL_LIGHTING);
+			Gl.glEnable(Gl.GL_LIGHT0);
+			Gl.glEnable(Gl.GL_NORMALIZE);
+			Gl.glLightModelf(Gl.GL_LIGHT_MODEL_TWO_SIDE, Gl.GL_TRUE);
+			Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_DIFFUSE, light_dif);
+		}
+	}
+}
